12006d29af358e912065380908f2fd22f2a2f152
xyloforce <fabsassolas@outlook.fr>
2020-12-04 15:29:19 +0100

    Previsions

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# CUISINE PLANIFICATOR
+
+## Objectifs :
+
+- obtenir un nombre de repas par semaine
+- un prix prévisionnel
+- pouvoir imposer d'autres critères (notamment variété)
+
+Demande de croiser trois infos :
+
+- prix par ingrédient
+- ingrédients par recette
+- recettes par semaine
+
+## Trois fonctions :
+
+- créer plan repas
+- ajouter recette dans db
+- ajouter ingrédient dans db
---
+++ script.py
@@ -0,0 +1,15 @@
+#### CUISINE PLANIFICATOR
+# objectifs :
+# - obtenir un nombre de repas par semaine
+# - un prix prévisionnel
+# - pouvoir imposer d'autres critères (notamment variété)
+
+# demande de croiser trois infos :
+# prix par ingrédient
+# ingrédients par recette
+# recettes par semaine
+
+# trois fonctions :
+# créer plan repas
+# ajouter recette dans db
+# ajouter ingrédient dans db
